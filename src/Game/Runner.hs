7c893a177b1208a915df8214527664ede85c406c
Przemysław Rogala <progala2@o2.pl>
2019-05-14 17:50:20 +0200

    Description printing

--- src/Game/Runner.hs
+++ src/Game/Runner.hs
@@ -5,15 +5,20 @@ import Game.CommandParser
 import qualified Control.Monad.State.Lazy as S
 import qualified Data.Map.Strict as M
 import Game.Types
+import Data.List
 
 runGame :: GameStateIO ()
 runGame = do
     S.lift $ clearScreen
-    ss <- S.get
+    ss@((PlayerStatus (LocName loc) _), _) <- S.get
+    putStrLnL $ "Location: " ++ loc
+    putStrLnL $ showDescription ss (currLoc ss)
+    putStrLnL "What now? : "
     line <- S.lift $ getLine
     printL $ S.evalState (either (\_ -> return "ss" ) handleCommand (parseCommand line)) ss
     runGame
-      --where 
+      where
+        currLoc ((PlayerStatus cL _), (_, locs)) = locs M.! cL 
         --rdHp opts k = case opts M.! k of Ge.GameOptionString str -> str; _ -> error "Unexpected error"
 
 handleCommand :: Command -> GameState String
@@ -27,5 +32,21 @@ handleCommand (Travel dest) = do
                     return $ "You travel to: " ++ (show dest)
                 Nothing -> return $ "There is no "++ (show dest) ++ "..." ++(show $ ((== dest) <$> M.keys lc))
 handleCommand (ItemsOnObject items obj) = return $ "Not yet implemented" ++ (show items) ++ (show obj)
+
+showDescription :: GameStatus -> Location -> String
+showDescription _ l = foldl' sepByLn [] $ desc <$> M.elems (lcDescList l)
+  where
+    desc (LocDesc str) = str
+    desc (LocDescCond _ _ str) = str
+
 printL :: (Show a) => a -> GameStateIO ()
 printL str = S.lift $ print str
+--putStrL :: String -> GameStateIO ()
+--putStrL str = S.lift $ putStr str
+putStrLnL :: String -> GameStateIO ()
+putStrLnL str = S.lift $ putStrLn str
+sepByLn :: String -> String -> String
+sepByLn = sepBy "\n" (++)
+
+sepBy :: a -> (a -> a -> a) -> a -> a -> a
+sepBy s f a b = a `f` s `f` b
